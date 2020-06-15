80785a96f4925bbe0d1a142297aef17c56e36111
Hikachi <hikachi420@gmail.com>
2020-06-15 15:06:50 +0200

    Plansza i komunikaty

--- app/src/main/java/com/example/myapplication/Activity_game.kt
+++ app/src/main/java/com/example/myapplication/Activity_game.kt
@@ -2,9 +2,12 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import androidx.core.view.isVisible
 import com.example.myapplication.Game_Board_View.PositionClickedListener
 import kotlinx.android.synthetic.main.activity_game.*
+import java.util.*
 
 class Activity_game : AppCompatActivity(), PositionClickedListener {
 
@@ -25,7 +28,63 @@ class Activity_game : AppCompatActivity(), PositionClickedListener {
         TODO("UNDO MOVE")
     }
     fun restartGameClick(view: View){
-        TODO("RESTART GAME")
+
+        showYourMove()
+        showPlayer1Move()
+        showPlayer2Move()
+        showPlayer1Wins()
+        showPlayer2Wins()
+        showAiWins()
+        showYouWin()
+    }
+    fun showYourMove(){
+        image_game_notification.setImageResource(R.drawable.your_move)
+        image_game_notification.visibility=View.VISIBLE
+        Handler().postDelayed({
+            image_game_notification.visibility = View.INVISIBLE
+        }, 700)
+    }
+    fun showPlayer1Move(){
+        image_game_notification.setImageResource(R.drawable.player1_move)
+        image_game_notification.visibility=View.VISIBLE
+        Handler().postDelayed({
+            image_game_notification.visibility = View.INVISIBLE
+        }, 700)
+    }
+    fun showPlayer1Wins(){
+        image_game_notification.setImageResource(R.drawable.player1_wins)
+        image_game_notification.visibility=View.VISIBLE
+        Handler().postDelayed({
+            image_game_notification.visibility = View.INVISIBLE
+        }, 2000)
+    }
+    fun showPlayer2Move(){
+        image_game_notification.setImageResource(R.drawable.player2_move)
+        image_game_notification.visibility=View.VISIBLE
+        Handler().postDelayed({
+            image_game_notification.visibility = View.INVISIBLE
+        }, 700)
+    }
+    fun showPlayer2Wins(){
+        image_game_notification.setImageResource(R.drawable.player2_wins)
+        image_game_notification.visibility=View.VISIBLE
+        Handler().postDelayed({
+            image_game_notification.visibility = View.INVISIBLE
+        }, 2000)
+    }
+    fun showAiWins(){
+        image_game_notification.setImageResource(R.drawable.ai_wins)
+        image_game_notification.visibility=View.VISIBLE
+        Handler().postDelayed({
+            image_game_notification.visibility = View.INVISIBLE
+        }, 2000)
+    }
+    fun showYouWin(){
+        image_game_notification.setImageResource(R.drawable.your_move)
+        image_game_notification.visibility=View.VISIBLE
+        Handler().postDelayed({
+            image_game_notification.visibility = View.INVISIBLE
+        }, 2000)
     }
     fun test (){
         val gameBoardArray = arrayOf(
